430e0d8cb63893dd7a28d7f61ae5b0a2c40d7d92
Alexey Belov <Alexey.Belov@x-tensive.com>
2022-08-26 14:24:23 +0500

    - enterprise structure: generate update actions

--- scripts/enterpriseStruct.psm1
+++ scripts/enterpriseStruct.psm1
@@ -65,7 +65,7 @@ function EnterpriseStruct-Configuration-DumpEnterprise([object] $enterpriseCfg)
 function EnterpriseStruct-ReadConfiguration()
 {
     $structCfgFileName = $PSScriptRoot + "/../data/enterpriseStruct.json"
-    $enterpriseCfg = Get-Content $structCfgFileName | Out-String | ConvertFrom-Json
+    $enterpriseCfg = Get-Content $structCfgFileName | Out-String | ConvertFrom-Json -AsHashtable
     EnterpriseStruct-Configuration-DumpEnterprise $enterpriseCfg
     return $enterpriseCfg;
 }
@@ -131,6 +131,243 @@ function EnterpriseStruct-Fetch()
     }
     EnterpriseStruct-FetchNode 0 "0" $existentCfgContainer
     foreach ($enterpriseCfg in $existentCfgContainer.enterprises) { EnterpriseStruct-Configuration-DumpEnterprise $enterpriseCfg }
+    return $existentCfgContainer.enterprises
+}
+
+function EnterpriseStructure-GenerateRemoveActions-workCenter($workCenterCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "RemoveWorkCenter"
+        id = $workCenterCfg.id
+        name = $workCenterCfg.name
+    }
+}
+
+function EnterpriseStructure-GenerateRemoveActions-storageZone($storageZoneCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "RemoveStorageZone"
+        id = $storageZoneCfg.id
+        name = $storageZoneCfg.name
+    }
+}
+
+function EnterpriseStructure-GenerateRemoveActions-department($departmentCfg, [ref] $actions)
+{
+    foreach ($subDepartmentCfg in $departmentCfg.departments) { EnterpriseStructure-GenerateRemoveActions-department $subDepartmentCfg $actions }
+    foreach ($workCenterCfg in $departmentCfg.workCenters) { EnterpriseStructure-GenerateRemoveActions-workCenter $workCenterCfg $actions }
+    foreach ($storageZoneCfg in $departmentCfg.storageZones) { EnterpriseStructure-GenerateRemoveActions-storageZone $storageZoneCfg $actions }
+    $actions.Value += @{
+        action = "RemoveDepartment"
+        id = $departmentCfg.id
+        name = $departmentCfg.name
+    }
+}
+
+function EnterpriseStructure-GenerateRemoveActions-site($siteCfg, [ref] $actions)
+{
+    foreach ($departmentCfg in $siteCfg.departments) { EnterpriseStructure-GenerateRemoveActions-department $departmentCfg $actions }
+    $actions.Value += @{
+        action = "RemoveSite"
+        id = $siteCfg.id
+        name = $siteCfg.name
+    }
+}
+
+function EnterpriseStructure-GenerateRemoveActions-enterprise($enterpriseCfg, [ref] $actions)
+{
+    foreach ($siteCfg in $enterpriseCfg.sites) { EnterpriseStructure-GenerateRemoveActions-site $siteCfg $actions }
+    $actions.Value += @{
+        action = "RemoveEnterprise"
+        id = $enterpriseCfg.id
+        name = $enterpriseCfg.name
+    }
+}
+
+function EnterpriseStructure-GenerateCreateActions-workCenter($workCenterCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "CreateWorkCenter"
+        cfg = $workCenterCfg
+    }
+}
+
+function EnterpriseStructure-GenerateCreateActions-storageZone($storageZoneCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "CreateStorageZone"
+        cfg = $storageZoneCfg
+    }
+}
+
+function EnterpriseStructure-GenerateCreateActions-department($departmentCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "CreateDepartment"
+        cfg = $departmentCfg
+    }
+    foreach ($subDepartmentCfg in $departmentCfg.departments) {
+        EnterpriseStructure-GenerateCreateActions-department $subDepartmentCfg $actions
+    }
+    foreach ($workCenterCfg in $departmentCfg.workCenters) {
+        EnterpriseStructure-GenerateCreateActions-workCenter $workCenterCfg $actions
+    }
+    foreach ($storageZoneCfg in $departmentCfg.storageZones) {
+        EnterpriseStructure-GenerateCreateActions-storageZone $storageZoneCfg $actions
+    }
+}
+
+function EnterpriseStructure-GenerateCreateActions-site($siteCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "CreateSite"
+        cfg = $siteCfg
+    }
+    foreach ($departmentCfg in $siteCfg.departments) {
+        EnterpriseStructure-GenerateCreateActions-department $departmentCfg $actions
+    }
+}
+
+function EnterpriseStructure-GenerateCreateActions-enterprise($enterpriseCfg, [ref] $actions)
+{
+    $actions.Value += @{
+        action = "CreateEnterprise"
+        cfg = $enterpriseCfg
+    }
+    foreach ($siteCfg in $enterpriseCfg.sites) {
+        EnterpriseStructure-GenerateCreateActions-site $siteCfg $actions
+    }
+}
+
+function EnterpriseStructure-GenerateUpdateActions-workCenters([ref] $workCentersCfg, [ref] $existentWorkCentersCfg, [ref] $actions)
+{
+    foreach ($existentWorkCenterCfg in $existentWorkCentersCfg.Value) {
+        $workCenterCfg = $workCentersCfg.Value | where { $_.name -eq $existentWorkCenterCfg.name }
+        if (-not $workCenterCfg) {
+            EnterpriseStructure-GenerateRemoveActions-workCenter $existentWorkCenterCfg $actions
+        }
+    }
+
+    foreach ($workCenterCfg in $workCentersCfg.Value) {
+        $existentWorkCenterCfg = $existentWorkCentersCfg.Value | where { $_.name -eq $workCenterCfg.name }
+        if ($existentWorkCenterCfg) {
+            $workCenterCfg.id = $existentWorkCenterCfg.id
+        }
+    }
+
+    foreach ($workCenterCfg in $workCentersCfg.Value) {
+        if (-not $workCenterCfg.ContainsKey("id")) {
+            EnterpriseStructure-GenerateCreateActions-workCenter $workCenterCfg $actions
+        }
+    }
+}
+
+function EnterpriseStructure-GenerateUpdateActions-storageZones([ref] $storageZonesCfg, [ref] $existentStorageZonesCfg, [ref] $actions)
+{
+    foreach ($existentStorageZoneCfg in $existentStorageZonesCfg.Value) {
+        $storageZoneCfg = $storageZonesCfg.Value | where { $_.name -eq $existentStorageZoneCfg.name }
+        if (-not $storageZoneCfg) {
+            EnterpriseStructure-GenerateRemoveActions-storageZone $existentStorageZoneCfg $actions
+        }
+    }
+
+    foreach ($storageZoneCfg in $storageZonesCfg.Value) {
+        $existentStorageZoneCfg = $existentStorageZonesCfg.Value | where { $_.name -eq $storageZoneCfg.name }
+        if ($existentStorageZoneCfg) {
+            $storageZoneCfg.id = $existentStorageZoneCfg.id
+        }
+    }
+
+    foreach ($storageZoneCfg in $storageZonesCfg.Value) {
+        if (-not $storageZoneCfg.ContainsKey("id")) {
+            EnterpriseStructure-GenerateCreateActions-storageZone $storageZoneCfg $actions
+        }
+    }
+}
+
+function EnterpriseStructure-GenerateUpdateActions-departments([ref] $departmentsCfg, [ref] $existentDepartmentsCfg, [ref] $actions)
+{
+    foreach ($existentDepartmentCfg in $existentDepartmentsCfg.Value) {
+        $departmentCfg = $departmentsCfg.Value | where { $_.name -eq $existentDepartmentCfg.name }
+        if (-not $departmentCfg) {
+            EnterpriseStructure-GenerateRemoveActions-department $existentDepartmentCfg $actions
+        }
+    }
+
+    foreach ($departmentCfg in $departmentsCfg.Value) {
+        $existentDepartmentCfg = $existentDepartmentsCfg.Value | where { $_.name -eq $departmentCfg.name }
+        if ($existentDepartmentCfg) {
+            $departmentCfg.id = $existentDepartmentCfg.id
+            EnterpriseStructure-GenerateUpdateActions-departments ([ref]$departmentCfg.departments) ([ref]$existentDepartmentCfg.departments) $actions
+            EnterpriseStructure-GenerateUpdateActions-workCenters ([ref]$departmentCfg.workCenters) ([ref]$existentDepartmentCfg.workCenters) $actions
+            EnterpriseStructure-GenerateUpdateActions-storageZones ([ref]$departmentCfg.storageZones) ([ref]$existentDepartmentCfg.storageZones) $actions
+        }
+    }
+
+    foreach ($departmentCfg in $departmentsCfg.Value) {
+        if (-not $departmentCfg.ContainsKey("id")) {
+            EnterpriseStructure-GenerateCreateActions-department $departmentCfg $actions
+        }
+    }
+}
+
+function EnterpriseStructure-GenerateUpdateActions-sites([ref] $sitesCfg, [ref] $existentSitesCfg, [ref] $actions)
+{
+    foreach ($existentSiteCfg in $existentSitesCfg.Value) {
+        $siteCfg = $sitesCfg.Value | where { $_.name -eq $existentSiteCfg.name }
+        if (-not $siteCfg) {
+            EnterpriseStructure-GenerateRemoveActions-site $existentSiteCfg $actions
+        }
+    }
+
+    foreach ($siteCfg in $sitesCfg.Value) {
+        $existentSiteCfg = $existentSitesCfg.Value | where { $_.name -eq $siteCfg.name }
+        if ($existentSiteCfg) {
+            $siteCfg.id = $existentSiteCfg.id
+            EnterpriseStructure-GenerateUpdateActions-departments ([ref]$siteCfg.departments) ([ref]$existentSiteCfg.departments) $actions
+        }
+    }
+
+    foreach ($siteCfg in $sitesCfg.Value) {
+        if (-not $siteCfg.ContainsKey("id")) {
+            EnterpriseStructure-GenerateCreateActions-site $siteCfg $actions
+        }
+    }
+}
+
+function EnterpriseStructure-GenerateUpdateActions($enterpriseCfg, $existentCfg)
+{
+    $actions = @()
+
+    foreach ($existentEnterpriseCfg in $existentCfg) {
+        if ($enterpriseCfg.name -ne $existentEnterpriseCfg.name) {
+            EnterpriseStructure-GenerateRemoveActions-enterprise $existentEnterpriseCfg ([ref]$actions)
+        }
+    }
+
+    foreach ($existentEnterpriseCfg in $existentCfg) {
+        if ($enterpriseCfg.name -eq $existentEnterpriseCfg.name) {
+            $enterpriseCfg.id = $existentEnterpriseCfg.id
+            EnterpriseStructure-GenerateUpdateActions-sites ([ref]$enterpriseCfg.sites) ([ref]$existentEnterpriseCfg.sites) ([ref]$actions)
+        }
+    }
+
+    if (-not $enterpriseCfg.ContainsKey("id")) {
+        EnterpriseStructure-GenerateCreateActions-enterprise $enterpriseCfg ([ref]$actions)
+    }
+
+    foreach ($action in $actions) {
+        if ($action.action.StartsWith("Remove")) {
+            Write-Host $action.action -NoNewLine -Foreground yellow
+            Write-Host (" " + $action.id + ":" + $action.name)
+        }
+        if ($action.action.StartsWith("Create")) {
+            Write-Host $action.action -NoNewLine
+            Write-Host (" " + $action.cfg.name)
+        }
+    }
+
+    return $actions
 }
 
 function EnterpriseStruct-Update()
@@ -144,4 +381,11 @@ function EnterpriseStruct-Update()
     Write-Host "enterprise struct FETCH" -Foreground green
     Write-Host
     $existentCfg = EnterpriseStruct-Fetch
+
+    Write-Host
+    Write-Host "enterprise struct UPDATE ACTIONS" -Foreground green
+    Write-Host
+    $updateActions = EnterpriseStructure-GenerateUpdateActions $enterpriseCfg $existentCfg
+
+    Write-Host ($enterpriseCfg | ConvertTo-Json -Depth 100)
 }
