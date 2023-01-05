d6c8df1e7dce08290bea821a3bb313f51003f764
Alexey Belov <Alexey.Belov@x-tensive.com>
2023-01-05 21:04:29 +0200

    - chalk - console colorer

--- src/dpaImport.ts
+++ src/dpaImport.ts
@@ -1,10 +1,11 @@
 import { dpa } from "./dpa.js";
+import chalk from "chalk";
 
 export type dpaImportTarget = "enterpriseStruct" | "shiftTemplates" | "shifts";
 
 export class dpaImport {
     static run(target: dpaImportTarget, client: dpa): void
     {
-        console.log("IMPORT", target);
+        console.log(chalk.blueBright("IMPORT", target));
     }
 }
